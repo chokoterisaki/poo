48f3fa18e3a695da23b51d134504abd4d6e63ecd
mateoroja <mateorojas2007alvarez@gmail.com>
2024-11-15 14:06:11 -0300

    avanze ej17

---
+++ ejerciciosObligatorios/ej17/BarajaEspañola.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ej17
+{
+    class BarajaEspañola : Baraja
+    {
+        bool jugar = false;
+        public BarajaEspañola(bool jugar, int cantT, int cartP):base(cantT, cartP) 
+        {
+
+        }    
+    }
+}
---
+++ ejerciciosObligatorios/ej17/Carta.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ej17
+{
+    internal class Carta
+    {
+        int num;
+        string palo;
+        enum PalosBarEspañola
+        {
+            OROS,
+            COPAS,
+            ESPADAS,
+            BASTOS
+        }
+        enum PalosBarFrancesa
+        {
+            DIAMANTES,
+            PICAS,
+            CORAZONES,
+            TREBOLES
+        }
+        public int Num { get { return num; } set { num = value; } }
+        public string Palo { get { return palo; } set { palo = value; } }
+
+        public Carta(int N, string P)
+        {
+            num = N;
+            palo = P;
+        }
+    }
+}
